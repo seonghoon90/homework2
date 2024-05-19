6ee6761694ad7220187c3070c0c705aac6f777af
hoon <dotofi@naver.com>
2024-05-19 21:31:12 +0900

    일정 작성 API 코드 추가

---
+++ src/main/java/com/sparta/homework2/controller/TodoController.java
@@ -0,0 +1,15 @@
+package com.sparta.homework2.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+
+    @PostMapping("/v1.0/todo")
+    public ResponseEntity postTodo(){
+        // TODO 일정 작성 기능
+        return ResponseEntity.ok().build();
+    }
+}
